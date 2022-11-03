39e8b3b041a52b6056821613b87971460179a2b0
Alex <alextr_r@outlook.com>
2022-11-03 19:39:26 -0300

    designing card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Card from './components/Card';
 import cardImageCarousel from './media/svg/carousel-image-0.svg';
 import cardImageCapacitaciones from './media/svg/capacitacionesImg.svg'
+import cardImageAcompañamiento from './media/svg/acompañamientoImage.svg'
 
 function App() {
 	return (
@@ -18,6 +19,13 @@ function App() {
 				img={cardImageCapacitaciones}
 				title="Capacitaciones"
 				description="Facilitamos capacitaciones en diversidad, género y discapacidad para empresas y organizaciones."
+				link="Saber mas"
+			/>
+
+<Card
+				img={cardImageAcompañamiento}
+				title="Acompañamiento"
+				description="Colaboramos con personas de la comunidad a sortear situaciones complejas."
 				link="Saber mas"
 			/>
 
