b5555f75ae6592741ce532beea1ebbc87a419b86
Alex <alextr_r@outlook.com>
2022-11-03 10:41:43 -0300

    Carga de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Card from './components/Card';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+			<Card 
+				title="Cursos y talleres"
+				description="loremLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum"
+				link="Saber mas"
+			/>
+		</div>
+	);
+}
+
+export default App;
