154bd520df50783c976bd16852925fb5a011d60c
Alex <alextr_r@outlook.com>
2022-12-01 16:59:35 -0300

    Update Nosotres View

--- src/views/Nosotres.jsx
+++ src/views/Nosotres.jsx
@@ -1,7 +1,73 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+
+import Footer from '../components/Footer';
+import Card from "../components/Card";
+
+
+
+
 function Nosotres(){
     return (
         <div className="nosotres">
-            <h1>Nosotres</h1>
+
+            <div className="hero-section">
+                <h1>Nosotres</h1>
+                <p className="hero-section__description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suspendisse suscipit elit ultricies risus arcu tellus. A, tellus tincidunt tortor, et cras non pretium urna. Risus dolor mi, amet dui dictum et condimentum. 
+                </p>
+            </div>
+
+            <div className="cards-section">
+
+                <Swiper
+                    modules={[Pagination]}
+                    spaceBetween={8}
+                    pagination={{ clickable: true }}
+                >
+                    <SwiperSlide>
+                        <Card
+                            //img={cardTalleres}
+                            alt="Descripcion de la imagen"
+                            title="Equipo de cominucación"
+                            description="Breve descripción de lo que hace el equipo en dos o tres lineas"
+                            link="Me quiero sumar"
+                        />
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        <Card
+                            //img={cardCapacitaciones}
+                            alt="Descripcion de la imagen"
+                            title="Equipo Educación"
+                            description="Breve descripción de lo que hace el equipo en dos o tres líneas."
+                            link="Me quiero sumar"
+                        />
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        <Card
+                            //img={cardAcompaniamiento}
+                            alt="Descripcion de la imagen"
+                            title="Equipo Diseño"
+                            description="Breve descripción de lo que hace el equipo en dos o tres líneas."
+                            link="Me quiero sumar"
+                        />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+
+            <div className="members-section">
+                <h2 className="members-section__title">Integrantes de Transistemas</h2>
+
+            </div>
+
+            <Footer />
+
+
+            
+
+
         </div>
     );
 }
