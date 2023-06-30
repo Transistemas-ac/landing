5e0c2f2e0123cd027aaecff61831d61d37b94338
Alex <44349881+ValdezAlex@users.noreply.github.com>
2023-06-30 13:16:51 -0300

    add members Array

---
+++ src/utils/members.js
@@ -0,0 +1,69 @@
+let members = [
+
+    {   nombre: 'Abe Quiroga',
+        rol: 'Co-fundadore y presidente de Transistemas',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Maximiliano Monti',
+        rol: 'UX Content Designer',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Julián Landó',
+        rol: 'Tester QA',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Raysa Alanes', 
+        rol: 'Full Stack Developer',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Ailin Lezcano',
+        rol: 'UX Designer',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Camila Rodriguez Vera',
+        rol: 'Estudiante de Diseño Multimedial / Diseñadora UX/UI',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Guadalupe Saavedra',
+        rol: 'Vendedora de salón',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Jazmin de Zavaleta',
+        rol: 'Digital Designer',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Micaela Montero',
+        rol: 'UX Designer',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Micaela Paula Reynoso',
+        rol: 'Diseñadora Gráfica / Diseñadora Digital',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Veronica Frund',
+        rol: 'UX/UI Designer / UX Researcher',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Alex Valdez',
+        rol: 'Developer / QA Tester',
+        foto: 'fotito'
+    },
+
+    {   nombre: 'Martin Bejarano',
+        rol: 'Web Developerramador',
+        foto: 'fotito'
+    }
+
+]
+
