f92647c7c4429b7c770b1033aab64f2923695228
nady4 <nadyajerochim@gmail.com>
2026-01-29 16:48:17 -0300

    voluntaries

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -1,7 +1,7 @@
 const metrics = [
   { value: 28, title: "Cursos dictados" },
   { value: 500, title: "Ayudas sociales" },
-  { value: 10, title: "Voluntaries" },
+  { value: 9, title: "Voluntaries" },
   { value: 1821, title: "Egresades" },
   { value: "+350", title: "Personas consiguieron trabajo registrado" },
 ];
