0770aa69249a38f3c6a80e286d79c11d4749abc2
nady4 <nadyajerochim@gmail.com>
2026-03-08 01:22:11 -0300

    add sitemap, update robots, favicon, switch to BrowserRouter, and remove unused public templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Paths } from "./routes";
 import ErrorPage from "./views/ErrorPage";
 import DisplayContext from "./context/DisplayProvider";
