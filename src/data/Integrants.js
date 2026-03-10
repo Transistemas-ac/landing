0b25eb563e851b476957970840eb73ea3589c7cf
nady4 <dev@nady4.com>
2026-03-09 23:05:27 -0300

    integrantes, fix menu, fix footer, layout

--- src/data/Integrants.js
+++ src/data/Integrants.js
@@ -6,6 +6,7 @@ import {
   pic5,
   pic6,
   pic7,
+  pic8
 } from "../assets/integrantes/index.js";
 
 const integrants = [
@@ -14,50 +15,57 @@ const integrants = [
     role: "Tester QA",
     picture: pic1,
     team: "education",
-    href: "https://www.linkedin.com/in/julianlandoqa/",
+    href: "https://www.linkedin.com/in/julianlandoqa/"
   },
   {
     name: "Damian Copello",
     role: "Sr QA Analyst",
     picture: pic2,
     team: "education",
-    href: "https://www.linkedin.com/in/damiancopello/",
+    href: "https://www.linkedin.com/in/damiancopello/"
   },
   {
     name: "Jazmin de Zavaleta",
     role: "Digital Designer",
     picture: pic3,
     team: "design",
-    href: "https://www.linkedin.com/in/jazm%C3%ADn-de-zavaleta-1502a292/",
+    href: "https://www.linkedin.com/in/jazm%C3%ADn-de-zavaleta-1502a292/"
   },
   {
     name: "Nadya Jerochim",
     role: "Coordinadora del equipo y FullStack Developer",
     picture: pic4,
     team: "development",
-    href: "https://www.linkedin.com/in/nady4/",
+    href: "https://www.linkedin.com/in/nady4/"
   },
   {
     name: "Tamara Merele",
     role: "Community Manager",
     picture: pic5,
     team: "communication",
-    href: "https://www.linkedin.com/in/merele-c-tamara",
+    href: "https://www.linkedin.com/in/merele-c-tamara"
   },
   {
     name: "Larú Saldaña",
     role: "Developer y QA",
     picture: pic6,
     team: "development",
-    href: "https://www.linkedin.com/in/lar%C3%BA-salda%C3%B1a/",
+    href: "https://www.linkedin.com/in/lar%C3%BA-salda%C3%B1a/"
   },
   {
     name: "Soledad Martínez",
     role: "Fullstack Developer y QA",
     picture: pic7,
     team: "development",
-    href: "https://www.linkedin.com/in/msoledadmartinez88/",
+    href: "https://www.linkedin.com/in/msoledadmartinez88/"
   },
+  {
+    name: "Alexis Boscaroli",
+    role: "Developer",
+    picture: pic8,
+    team: "development",
+    href: "https://www.linkedin.com/in/alexis-boscaroli/"
+  }
 ];
 
 export default integrants;
